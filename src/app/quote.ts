66dca3f0e736075daf78acf5d1c9d2e97cb0b02a
Annewangari <annekare3@gmail.com>
2020-09-25 18:39:52 +0300

    difine logic in qoute class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,12 @@
 export class Quote {
-}
+    showDescription= false;
+
+    constructor(
+       id: number,
+       name: string,
+       description: string,
+       submitter: string,
+       completeDate: Date) {
+  }
+    } 
+
