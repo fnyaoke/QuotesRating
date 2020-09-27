c6aa9cb22f8ba2ed73e491b3bdb02e8143563a90
Annewangari <annekare3@gmail.com>
2020-09-27 22:50:50 +0300

    create a function get highest upvote to get the quote with the highest upvote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -40,6 +40,19 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index) {
     this.quotes[index].showDetails = !this.quotes[index].showDetails;
   }
+  getHighestUpVote(): Quote {
+    let highestUpVoteQuote = new Quote(0, '', '', '', new Date());
+    for (let i = 0; i < this.quotes.length; i++) {
+      if (this.quotes[i].upvote > highestUpVoteQuote.upvote) {
+        highestUpVoteQuote = this.quotes[i];
+      }
+    }
+    if (highestUpVoteQuote.upvote > 0) {
+      return highestUpVoteQuote;
+    } else{
+      return new Quote(0, '', '', '', new Date());
+    }
+  }
   constructor() { }
 
   ngOnInit() {
